905b87f52ad0bf46599d26d23952e7d3c33895b3
Crowdbotics <team@crowdbotics.com>
2020-10-18 01:55:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/shoot_fun_21648/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shoot_fun_21648;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "shoot_fun_21648";
+    }
+}
